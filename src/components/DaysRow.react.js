debbea918e680bbf0c5b68f4381b442888bd1f03
Pedro Santos <donbonifacio@gmail.com>
2016-01-13 14:21:48 +0000

    Refactor, ask for full days coll on lib

--- src/components/DaysRow.react.js
+++ src/components/DaysRow.react.js
@@ -3,17 +3,6 @@
 var React = require('react');
 var _ = require('underscore');
 var moment = require('moment');
-var MAX_WEEK_DAYS = 7;
-
-function daysAsCollection(start, end) {
-  var current = moment(start);
-  var days = [start];
-  while(days.length < MAX_WEEK_DAYS) {
-    var anotherDay = current.add(1, 'days');
-    days.push(moment(anotherDay));
-  }
-  return days;
-}
 
 function getDayClassName(day) {
   var isoWeekday = day.isoWeekday();
@@ -28,7 +17,7 @@ function getDayClassName(day) {
 module.exports = React.createClass({
   render: function render() {
     var data = this.props.data;
-    var days = daysAsCollection(data.startDay, data.endDay);
+    var days = data.days;
     return (
       <tr className="monthDay">
         {_.map(days, function daysToComponent(day, key) {
--- src/demo.js
+++ src/demo.js
@@ -8,8 +8,13 @@ var Calendar = require('./main.js');
 var rawData = {
   weeks: [
     {
-      startDay: moment("2015-12-27"),
-      endDay: moment("2016-01-02")
+      days: [moment("2015-12-27"),
+             moment("2015-12-28"),
+             moment("2015-12-29"),
+             moment("2015-12-30"),
+             moment("2015-12-31"),
+             moment("2016-01-01"),
+             moment("2016-01-02")]
     }
 
   ],
