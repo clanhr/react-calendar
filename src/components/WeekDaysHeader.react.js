74b7d37d7c3c2de64e38e1f2964fc5a373d37748
Mariana Barros <mariana.barros@rupeal.com>
2016-01-14 09:59:07 +0000

    created css style for demo page

--- src/components/WeekDaysHeader.react.js
+++ src/components/WeekDaysHeader.react.js
@@ -9,13 +9,13 @@ module.exports = React.createClass({
       <table className="table weekDays">
         <thead>
           <tr>
-            <th className="smallTitle text-center">Seg</th>
-            <th className="smallTitle text-center">Ter</th>
-            <th className="smallTitle text-center">Qua</th>
-            <th className="smallTitle text-center">Qui</th>
-            <th className="smallTitle text-center">Sex</th>
-            <th className="smallTitle text-center">Sab</th>
-            <th className="smallTitle text-center">Dom</th>
+            <th className="text-center">Seg</th>
+            <th className="text-center">Ter</th>
+            <th className="text-center">Qua</th>
+            <th className="text-center">Qui</th>
+            <th className="text-center">Sex</th>
+            <th className="text-center">Sab</th>
+            <th className="text-center">Dom</th>
           </tr>
         </thead>
       </table>
