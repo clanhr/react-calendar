b5ff71f3f6920abe8d2bdd30c7cd64f057db57b7
Pedro Santos <donbonifacio@gmail.com>
2016-01-14 16:15:35 +0000

    Complete summary display

--- src/components/Summary.react.js
+++ src/components/Summary.react.js
@@ -5,8 +5,10 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function render() {
-    // TODO use data
-    var content = "<div class='eventSpan'><div class='content'><p><span class='status approved'></span><strong>Rui Alves</strong> - Type of event <span class='dateRange'>(13/01/2015 a 18/01/2015)</span></p></div></div>";
+    var content = _.map(this.props.data.events, function eventToStr(event) {
+      return "<div class='eventSpan'><div class='content'><p><span class='status "+event.status+"'></span><strong>"+event.label+"</strong> - "+event.type+" <span class='dateRange'>"+event.detail+"</span></p></div></div>";
+    }).join("");
+
     return (
       <button type="button" className="popoverButton" data-trigger="hover" data-toggle="popover" data-content={content}>{this.props.data.label}</button>
     );
